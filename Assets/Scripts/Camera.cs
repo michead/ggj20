dc10e97e45401206f41b718d994e70ede8408d2e
michead <simone.riccardelli@gmail.com>
2020-02-01 03:40:14 +0000

    integrate character placeholders in main map

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public float boomMultiplier = 1.0f;
+
+    private GameObject player1;
+    private GameObject player2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player1 = GameObject.FindGameObjectWithTag("player1");
+        player2 = GameObject.FindGameObjectWithTag("player2");
+
+        UpdateTransform();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateTransform();
+    }
+
+    private void UpdateTransform()
+    {
+        var target = (player1.transform.position + player2.transform.position) / 2;
+        var direction = (target - transform.position).normalized;
+        var lookAtRotation = Quaternion.LookRotation(direction);
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookAtRotation, Time.deltaTime * boomMultiplier);
+    }
+}
