55732fed6a985d6c7ac4127fcdd980879e599c95
Paulius Bieksa <priverstinis@gmail.com>
2020-02-02 12:21:29 +0000

    Countdown clock.

---
+++ Assets/Scripts/Clock.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clock : MonoBehaviour
+{
+    float time = 20.0f;
+    [SerializeField]
+    TextMesh tm;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time = Mathf.Clamp(time - Time.deltaTime, 0.0f, 9999.0f);
+
+        string text = "";
+        if (time >= 10.0f)
+            text = time.ToString("F2");
+        else
+            text = "0" + time.ToString("F2");
+        text = text.Replace('.', ':');
+        tm.text = text;
+    }
+
+    void Add_time(float t)
+    {
+        time += t;
+    }
+}
